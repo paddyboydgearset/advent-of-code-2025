7f53fdebf80c823f1c0b9e65973536d77da43115
Patrick Boyd <patrick.boyd@gearset.com>
2025-12-03 08:10:57 +0000

    DayThree

---
+++ dayone/DayThree.cs
@@ -0,0 +1,82 @@
+using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+using advent_of_code_2025.Loader;
+
+namespace advent_of_code_2025.dayone;
+
+public class DayThree
+{
+    private IEnumerable<long[]> GetLines()
+    {
+        foreach (var line in InputLoader.IterateEmbeddedResourceLines(@"day-three.txt"))
+        {
+            yield return line.Select(c => Int64.Parse(c.ToString())).ToArray();
+        }
+    }
+    
+    public void SolvePartOne()
+    {
+        long maxAmount = 0;
+        
+        foreach (var lineArray in GetLines())
+        {
+            maxAmount += FindMax(lineArray);
+        }
+        
+        Console.WriteLine($"Max joltage: {maxAmount}");
+    }
+    
+    public void SolvePartTwo()
+    {
+        long maxAmount = 0;
+        
+        foreach (var lineArray in GetLines())
+        {
+            maxAmount += FindMax(lineArray, 12);
+        }
+        
+        Console.WriteLine($"Max joltage: {maxAmount}");
+    }
+
+    private long FindMax(long[] digits)
+    {
+        List<long> excludeFromMax = [];
+
+        while (excludeFromMax.Count < 9)
+        {
+            var maxDigit = digits.Except(excludeFromMax).Max();
+            var firstIndex = Array.IndexOf(digits, maxDigit);
+            if (firstIndex != digits.Length - 1)
+            {
+                var maxNext = digits.Skip(firstIndex + 1).Max();
+                return Int64.Parse($"{maxDigit}{maxNext}");
+            }
+
+            excludeFromMax.Add(maxDigit);
+        }
+
+        return 0;
+    }
+    
+    private long FindMax(long[] digits, int finalDigitCount)
+    {
+        List<long> excludeFromMax = [];
+
+        if (finalDigitCount == 1) return digits.Max();
+
+        while (excludeFromMax.Count < 9)
+        {
+            var maxDigit = digits.Except(excludeFromMax).Max();
+            var firstIndex = Array.IndexOf(digits, maxDigit);
+            if (firstIndex <= digits.Length - (finalDigitCount))
+            {
+                var maxNext = FindMax(digits.Skip(firstIndex + 1).ToArray(), finalDigitCount - 1);
+                return Int64.Parse($"{maxDigit}{maxNext}");
+            }
+
+            excludeFromMax.Add(maxDigit);
+        }
+
+        return 0;
+    }
+}
